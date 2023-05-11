b82022e9f02ddb260da0ee0285868e04e7573608
eissa009 <129591969+eissa009@users.noreply.github.com>
2023-05-11 06:58:21 +0300

    Add files via upload

---
+++ flowchart.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    std::cout << "+-----------------------+\n";
+    std::cout << "| Read data from file    |\n";
+    std::cout << "+-----------+-----------+\n";
+    std::cout << "            |\n";
+    std::cout << "            v\n";
+    std::cout << "+-----------+-----------+\n";
+    std::cout << "| Calculate the equations |\n";
+    std::cout << "| for each data |\n";
+    std::cout << "| point                  |\n";
+    std::cout << "+-----------+-----------+\n";
+    std::cout << "            |\n";
+    std::cout << "            v\n";
+    std::cout << "+-----------+-----------+\n";
+    std::cout << "| Calculate I-V curve    |\n";
+    std::cout << "+-----------+-----------+\n";
+    std::cout << "            |\n";
+    std::cout << "            v\n";
+    std::cout << "+-----------+-----------+\n";
+    std::cout << "| Output data to file    |\n";
+    std::cout << "+-----------+-----------+\n";
+    return 0;
+}
